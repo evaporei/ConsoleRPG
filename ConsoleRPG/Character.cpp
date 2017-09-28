fd49c4fd2ce7156c7b13791ed4c7d2569d5c9dae
Otávio Pace <otaviopp8@gmail.com>
2017-09-28 16:58:06 -0300

    character: added basic members
    Added basic members and accessors
    to Character class

--- ConsoleRPG/Character.cpp
+++ ConsoleRPG/Character.cpp
@@ -4,9 +4,33 @@
 
 Character::Character()
 {
+	name = "";
+	level = 0;
+	experience = 0;
+	experienceForNextLevel = 0;
+	health = 0;
+	maximumHealth = 0;
+	stamina = 0;
+	minimumDamage = 0;
+	maximumDamage = 0;
+	defense = 0;
 }
 
 
 Character::~Character()
 {
+}
+
+void Character::initialize(int intialLevel, std::string initialName)
+{
+	name = initialName;
+	level = intialLevel;
+	experience = 0;
+	experienceForNextLevel = 100;
+	health = 10;
+	maximumHealth = 10;
+	stamina = 10;
+	minimumDamage = 2;
+	maximumDamage = 4;
+	defense = 1;
 }
--- ConsoleRPG/Character.h
+++ ConsoleRPG/Character.h
@@ -7,5 +7,36 @@ class Character
 public:
 	Character();
 	virtual ~Character();
+
+	void initialize(int intialLevel, std::string intialName);
+
+	inline const int& getXPosition() const { return xPosition; }
+	inline const int& getYPosition() const { return yPosition; }
+
+	inline const std::string& getName() const { return name; }
+	inline const int& getLevel() const { return level; }
+	inline const int& getExperience() const { return experience; }
+	inline const int& getExperienceForNextLevel() const { return experienceForNextLevel; }
+	inline const int& getHealth() const { return health; }
+	inline const int& getMaximumHealth() const { return maximumHealth; }
+	inline const int& getStamina() const { return stamina; }
+	inline const int& getMinimumDamage() const { return minimumDamage; }
+	inline const int& getMaximumDamage() const { return maximumDamage; }
+	inline const int& getDefense() const { return defense;  }
+
+private:
+	double xPosition;
+	double yPosition;
+
+	std::string name;
+	int level;
+	int experience;
+	int experienceForNextLevel;
+	int health;
+	int maximumHealth;
+	int stamina;
+	int minimumDamage;
+	int maximumDamage;
+	int defense;
 };
 
