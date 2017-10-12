8356971d3d73495982e6dd62c44c3cedc9985410
Otávio Pace <otaviopp8@gmail.com>
2017-10-12 09:48:19 -0300

    inventory: basic logic added
    Created basic logic to Inventory
    class.

--- ConsoleRPG/Inventory.cpp
+++ ConsoleRPG/Inventory.cpp
@@ -4,9 +4,63 @@
 
 Inventory::Inventory()
 {
+	this->capacity = 10;
+	this->numberOfItems = 0;
+	this->itemsArray = new Item*[this->capacity];
 }
 
 
 Inventory::~Inventory()
 {
+	for (int i = 0; i < this->numberOfItems; i++)
+	{
+		delete this->itemsArray[i];
+	}
+	delete[] itemsArray;
+}
+
+void Inventory::expand()
+{
+	this->capacity *= 2;
+
+	Item **tempArray = new Item*[this->capacity];
+
+	for (int i = 0; i < this->numberOfItems; i++)
+	{
+		tempArray[i] = new Item(*this->itemsArray[i]);
+	}
+
+	for (int i = 0; i < this->numberOfItems; i++)
+	{
+		delete this->itemsArray[i];
+	}
+
+	delete[] this->itemsArray;
+
+	this->itemsArray = tempArray;
+
+	this->initialize(this->numberOfItems);
+}
+
+void Inventory::initialize(const int from)
+{
+	for (int i = from; i < this->capacity; i++)
+	{
+		this->itemsArray[i] = nullptr;
+	}
+}
+
+void Inventory::addItem(const Item &item)
+{
+	if (this->numberOfItems >= this->capacity)
+	{
+		this->expand();
+	}
+
+	this->itemsArray[this->numberOfItems++] = new Item(item);
+}
+
+void Inventory::removeItem(int index)
+{
+
 }
--- ConsoleRPG/Inventory.h
+++ ConsoleRPG/Inventory.h
@@ -1,8 +1,18 @@
 #pragma once
+
+#include "Item.h"
 class Inventory
 {
+private:
+	int capacity;
+	int numberOfItems;
+	Item **itemsArray;
+	void expand();
+	void initialize(const int from);
 public:
 	Inventory();
 	virtual ~Inventory();
+	void addItem(const Item &item);
+	void removeItem(int index);
 };
 
