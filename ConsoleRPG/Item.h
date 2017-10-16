97d5355d2deac177eb3d370f01283bb266f6cb0d
Otávio Pace <otaviopp8@gmail.com>
2017-10-15 22:58:09 -0200

    item: added inline accessors for properties
    Added inline accessors/getters to the properties
    of Item class inside of Item.h file.

--- ConsoleRPG/Item.h
+++ ConsoleRPG/Item.h
@@ -13,5 +13,11 @@ private:
 public:
 	Item(std::string name, int level, int buyValue, int sellValue, int rarity);
 	~Item();
+
+	inline const std::string& getName() const { return this->name; }
+	inline const int& getLevel() const { return this->level; }
+	inline const int& getBuyLevel() const { return this->buyValue; }
+	inline const int& getSellValue() const { return this->sellValue; }
+	inline const int& getRarity() const { return this->rarity; }
 };
 
