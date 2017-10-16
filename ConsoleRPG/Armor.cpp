cc6b1066abd6aaea8d03835519df358a8753ced3
Otávio Pace <otaviopp8@gmail.com>
2017-10-16 00:37:13 -0200

    armor: added two properties and changed constructor
    Added two properties to Armor class and constructor
    now accepts parameters to build object.

--- ConsoleRPG/Armor.cpp
+++ ConsoleRPG/Armor.cpp
@@ -1,12 +1,16 @@
 #include "Armor.h"
 
-
-
-Armor::Armor()
+Armor::Armor(int type = 0, int defense = 0, std::string name = "NONE", int level = 0, int buyValue = 0, int sellValue = 0, int rarity = 0) : Item(name, level, buyValue, sellValue, rarity)
 {
+	this->type = type;
+	this->defense = defense;
 }
-
 
 Armor::~Armor()
 {
+}
+
+std::string Armor::getAsString()
+{
+	return std::to_string(this->type) + " " + std::to_string(this->defense);
 }
--- ConsoleRPG/Armor.h
+++ ConsoleRPG/Armor.h
@@ -3,8 +3,13 @@
 class Armor :
 	public Item
 {
+private:
+	int type;
+	int defense;
 public:
-	Armor();
+	Armor(int type, int defense, std::string name, int level, int buyValue, int sellValue, int rarity);
 	~Armor();
+
+	std::string getAsString();
 };
 
